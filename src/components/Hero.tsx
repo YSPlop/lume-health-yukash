603d8f5e48a56b9029e880c98bc95778111a27cd
zimop <zimopeng12345@gmail.com>
2025-02-13 09:18:05 +1100

    added responsive design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -41,12 +41,12 @@ const HeroButton = ({
       <motion.button 
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
-        className={`relative w-full px-4 md:px-8 py-3 md:py-6 ${buttonStyles[style].bgColor} ${buttonStyles[style].textColor} rounded-full font-semibold flex items-center justify-center gap-2 text-sm md:text-base overflow-hidden group`}
+        className={`relative w-full xl:px-5 xl:py-5 lg:px-3 lg:py-3 md:px-2 md:py-2 py-2 px-1 ${buttonStyles[style].bgColor} ${buttonStyles[style].textColor} rounded-full font-semibold flex items-center justify-center gap-2 text-sm md:text-base overflow-hidden group`}
       >
         <span className={`absolute inset-0 ${buttonStyles[style].hoverBgColor} scale-0 group-hover:scale-100 transition-transform duration-500 ease-out rounded-full origin-center`} />
         <span className="relative z-10 flex items-center justify-center gap-2 group-hover:text-white transition-colors duration-500 ease-out">
-          <Icon className="text-lg md:text-2xl" />
-          <p className="text-base md:text-2xl">{children}</p>
+          <Icon className="lg:text-[1.5vw] md:text-[1.6vw] sm:text-[2vw] text-[3vw]" />
+          <p className="lg:text-[1.2vw] md:text-[1.4vw] sm:text-[2vw] text-[3vw]">{children}</p>
         </span>
       </motion.button>
     </Link>
@@ -56,17 +56,16 @@ const HeroButton = ({
 const Hero = () => {
   return (
     <section className="p-2 md:p-6 lg:p-8 pt-6 md:pt-10 min-h-screen">
-      <div className="mx-auto rounded-[1.5rem] md:rounded-[2.5rem] bg-cardcolour backdrop-blur-sm p-3 md:p-6 flex flex-col md:max-h-[85vh] md:max-w-[70vw]">
-        <div className="px-2 md:px-12 py-3 md:py-5 flex flex-col justify-between">
-
+      <div className="mx-auto rounded-[1.5rem] md:rounded-[2.5rem] bg-cardcolour backdrop-blur-sm p-3 md:p-6 flex flex-col md:max-h-[85vw] md:max-w-[70vw] ">
+        <div className="px-4 lg:px-8 py-3 md:py-5 flex flex-col justify-between">
           <div className="grid md:grid-cols-2 gap-4 md:gap-0">
             {/* Left Content */}
             
-            <div className="px-2 ml-2 md:mt-20 mt-auto">
-              <h1 className={`text-3xl md:text-7xl lg:text-8xl font-heading mb-4 md:mb-11 text-center md:text-left ${styles.tagline}`}>
-                Quality Allied <br /> Health Services, <br /> Right at <br /> Your Door.
+            <div className="px-2 xl:mt-15 lg:mt-4 flex flex-col">
+              <h1 className={`lg:text-[3.7vw] md:text-[3.4vw] text-[5vw] font-heading mb-4 md:mb-11 text-center md:text-left ${styles.tagline}`}>
+                Quality Allied <span className="hidden md:inline"><br /></span> Health Services, <br /> Right at <span className="hidden md:inline"><br /></span> Your Door.
               </h1>
-              <p className="text-gray-600 text-sm md:text-[2rem] leading-[1.5] font-body mb-2 md:mb-20 md:mt-20 text-center md:text-left">
+              <p className="text-gray-600 lg:text-[1.2vw] text-[2.4vw] md:text-[1.5vw] leading-[1.5] font-body text-center md:text-left">
                 Lume Health offers mobile allied health services throughout Melbourne, catering to all ages and stages of life.
               </p>
             </div>
@@ -84,7 +83,7 @@ const Hero = () => {
           </div>
 
           {/* Buttons Section */}
-          <div className="grid md:grid-cols-3 gap-3 md:gap-8 mt-10 mb-10 px-2">
+          <div className="grid sm:grid-cols-3 gap-3 md:gap-8 lg:mt-16 md:mt-10 mt-4 px-2">
             <HeroButton href="/contact" style="contact">
               Contact Us
             </HeroButton>
